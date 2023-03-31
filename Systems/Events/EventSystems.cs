dea41ff3fe558abe1dd167ad179cd0999df6b901
Sergey Dyuchkov <jackkdlite@gmail.com>
2023-03-31 15:14:31 +0600

    Fix event systems

--- Systems/Events/EventSystems.cs
+++ Systems/Events/EventSystems.cs
@@ -1,69 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-using ModulesFramework.Data;
 
 namespace ModulesFramework.Systems.Events
 {
     internal class EventSystems
     {
-        private readonly Dictionary<Type, List<IEventSystem>> _systems = new Dictionary<Type, List<IEventSystem>>()
-        {
-            { typeof(IRunEventSystem), new List<IEventSystem>(64) },
-            { typeof(IPostRunEventSystem), new List<IEventSystem>(64) },
-            { typeof(IFrameEndEventSystem), new List<IEventSystem>(64) }
-        };
-
-        internal void AddSystem(IEventSystem eventSystem)
-        {
-            foreach (var type in _systems.Keys)
+        private readonly Dictionary<Type, List<IEventSystem>> _systems =
+            new Dictionary<Type, List<IEventSystem>>()
             {
-                if (type.IsInstanceOfType(eventSystem))
-                    _systems[type].Add(eventSystem);
-            }
+                { typeof(IRunEventSystem), new List<IEventSystem>(64) },
+                { typeof(IPostRunEventSystem), new List<IEventSystem>(64) },
+                { typeof(IFrameEndEventSystem), new List<IEventSystem>(64) }
+            };
+
+
+        internal void AddRunEventSystem(IEventSystem system)
+        {
+            _systems[typeof(IRunEventSystem)].Add(system);
         }
         
+        internal void AddPostRunEventSystem(IEventSystem system)
+        {
+            _systems[typeof(IPostRunEventSystem)].Add(system);
+        }
+        
+        internal void AddFrameEndEventSystem(IEventSystem system)
+        {
+            _systems[typeof(IFrameEndEventSystem)].Add(system);
+        }
+
         internal void HandleEvent<T>(T ev, Type eventSystemType) where T : struct
         {
-            if(eventSystemType == typeof(IRunEventSystem))
+            if (eventSystemType == typeof(IRunEventSystem))
             {
                 RunEvent(ev);
                 return;
             }
-            
+
             if (eventSystemType == typeof(IPostRunEventSystem))
             {
                 PostRunEvent(ev);
                 return;
             }
-            
+
             if (eventSystemType == typeof(IFrameEndEventSystem))
             {
                 FrameEndEvent(ev);
-                return;
             }
         }
 
-        internal void RunEvent<T>(T ev) where T : struct
+        private void RunEvent<T>(T ev) where T : struct
         {
             foreach (var system in _systems[typeof(IRunEventSystem)])
             {
-                ((IRunEventSystem<T>) system).RunEvent(ev);
+                ((IRunEventSystem<T>)system).RunEvent(ev);
             }
         }
-        
-        internal void PostRunEvent<T>(T ev) where T : struct
+
+        private void PostRunEvent<T>(T ev) where T : struct
         {
             foreach (var system in _systems[typeof(IPostRunEventSystem)])
             {
-                ((IPostRunEventSystem<T>) system).PostRunEvent(ev);
+                ((IPostRunEventSystem<T>)system).PostRunEvent(ev);
             }
         }
-        
-        internal void FrameEndEvent<T>(T ev) where T : struct
+
+        private void FrameEndEvent<T>(T ev) where T : struct
         {
             foreach (var system in _systems[typeof(IFrameEndEventSystem)])
             {
-                ((IFrameEndEventSystem<T>) system).FrameEndEvent(ev);
+                ((IFrameEndEventSystem<T>)system).FrameEndEvent(ev);
             }
         }
     }
