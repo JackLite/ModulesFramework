401b3832e3c8a1d06167f42789c9ac8693904628
Sergey Dyuchkov <jackkdlite@gmail.com>
2023-04-01 18:07:23 +0600

    Update assemblies

---
+++ AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("JackLite.ModulesFramework.UnityAdapter")]
+[assembly: InternalsVisibleTo("JackLite.ModulesFramework.UnityAdpater.Editor")]
