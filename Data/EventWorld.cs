16d09b1c0d2849777a85fff416b98fc2fe40fa04
JackLite <jackkdlite@gmail.com>
2025-04-08 10:08:04 +0300

    Add external listeners for events

--- Data/EventWorld.cs
+++ Data/EventWorld.cs
@@ -1,11 +1,14 @@
 using ModulesFramework.Modules;
 using System.Collections.Generic;
+using ModulesFramework.Data.Events;
+using ModulesFramework.Utils;
 
 namespace ModulesFramework.Data
 {
     public partial class DataWorld
     {
         private readonly List<EcsModule> _externalSubscribers = new(4);
+        private readonly Map<List<IExternalEventListener>> _externalListeners = new Map<List<IExternalEventListener>>();
 
         /// <summary>
         /// Create default event T and rise it
@@ -31,8 +34,16 @@ namespace ModulesFramework.Data
 #if MODULES_DEBUG
             Logger.LogDebug($"Rising {typeof(T).Name} event", LogFilter.EventsFull);
 #endif
-
             var wasHandled = false;
+
+            if (_externalListeners.TryGet<T>(out var listenersList))
+            {
+                foreach (var listener in listenersList)
+                    ((IExternalEventListener<T>)listener).OnEvent(ev);
+
+                wasHandled = true;
+            }
+
             foreach (var module in _externalSubscribers)
                 wasHandled |= HandleEvent(ev, module);
 
@@ -57,6 +68,34 @@ namespace ModulesFramework.Data
         internal void RegisterEventSubscriber(EcsModule module)
         {
             _externalSubscribers.Add(module);
+        }
+
+        public void RegisterListener<T>(IExternalEventListener<T> listener) where T : struct
+        {
+            if (!_externalListeners.TryGet<T>(out var list))
+            {
+                list = new List<IExternalEventListener>(4);
+                _externalListeners.Add<T>(list);
+            }
+
+            list.Add(listener);
+        }
+
+        public void UnregisterListener<T>(IExternalEventListener<T> listener) where T : struct
+        {
+            if (!_externalListeners.TryGet<T>(out var list))
+            {
+#if MODULES_DEBUG
+                Logger.LogWarning($"Listener {listener.GetType().Name} is not registered");
+#endif
+                return;
+            }
+
+            list.Remove(listener);
+            if (list.Count == 0)
+            {
+                _externalListeners.Remove<T>();
+            }
         }
     }
 }
