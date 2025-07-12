f53280a73ddee9681cb2b2e8b852667edcd537e9
JackLite <jackkdlite@gmail.com>
2025-07-12 15:45:47 +0200

    Add exception in debug mode when index already exists

---
+++ Exceptions/IndexAlreadyExistsException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ModulesFramework.Exceptions
+{
+    public class IndexAlreadyExistsException<T> : Exception where T : struct
+    {
+        public int EntityId { get; private set; }
+        public IndexAlreadyExistsException(int eid) : base($"Index for {typeof(T).Name} already exists for entity {eid}")
+        {
+            EntityId = eid;
+        }
+    }
+}
